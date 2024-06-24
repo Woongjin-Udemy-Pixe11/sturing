757d3a672392e8be675bec0e221369158a391090
suwan <skdoqj@naver.com>
2024-06-24 13:46:34 +0900

    [setting] db setting

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,13 @@
+import connectDB from "@/lib/db";
+import { User } from "@/lib/schema";
+
+export async function GET(req: Request) {
+  return Response.json({ message: "GET / api/users" });
+}
+
+export async function POST(request: Request) {
+  connectDB();
+  const res = await request.json();
+  const user = await User.create(res);
+  return Response.json(user);
+}
