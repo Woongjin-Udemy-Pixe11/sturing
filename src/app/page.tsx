e87f6c6ccc455e9af450ff8fbd5282b2b392a696
zzzzsb <ji12subin@gmail.com>
2024-06-25 09:52:32 +0900

    [design] 테스트 컴포넌트 삭제 feat: 새로운 기능 추가 fix: 버그 수정 docs: 문서 수정 design: 퍼블리싱, CSS 등 사용자 UI 디자인 변경 style: 들여쓰기, 세미콜론 등 코드 스타일 변경 refactor: 코드 리팩토링 chore: 외부 라이브러리 임포트 등의 작업 완료 comment: 필요한 주석 추가 및 변경 rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우 remove: 파일을 삭제하는 작업만 한 경우

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,6 @@ export default function page() {
   return (
     <>
       <h1>page Component</h1>
-      <Card
-        studyImage={'/images/card-dummy-img.png'}
-        studyMeetings={'스터디 시간 미정'}
-        studyTypeisBlue={true}
-        studyType={'오프라인'}
-        studyCategoryisBlue={false}
-        studyCatecory={'디자인'}
-        studyName={'AI 활용 UXUI 포트폴리오 강의 스터디'}
-        studyStart={'06.20'}
-        studyEnd={'06.30'}
-        studyPlace={'서울 종로구'}
-        studyJoinMember={2}
-        studyMember={4}
-      />
     </>
   );
 }
