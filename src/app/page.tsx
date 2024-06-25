9e212f04e1a345a1db34f4e5fa94cbd5d50c8cff
zzzzsb <ji12subin@gmail.com>
2024-06-25 09:15:41 +0900

    [design] Card 컴포넌트 퍼블리싱
    feat: 새로운 기능 추가
    fix: 버그 수정
    docs: 문서 수정
    design: 퍼블리싱, CSS 등 사용자 UI 디자인 변경
    style: 들여쓰기, 세미콜론 등 코드 스타일 변경
    refactor: 코드 리팩토링
    chore: 외부 라이브러리 임포트 등의 작업 완료
    comment: 필요한 주석 추가 및 변경
    rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    remove: 파일을 삭제하는 작업만 한 경우

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import Card from '@/components/common/Card';
+import Label from '@/components/common/Label';
+
 export default function page() {
   return (
     <>
       <h1>page Component</h1>
+      <Card
+        studyImage={'/images/card-dummy-img.png'}
+        studyMeetings={'스터디 시간 미정'}
+        studyTypeisBlue={true}
+        studyType={'오프라인'}
+        studyCategoryisBlue={false}
+        studyCatecory={'디자인'}
+        studyName={'AI 활용 UXUI 포트폴리오 강의 스터디'}
+        studyStart={'06.20'}
+        studyEnd={'06.30'}
+        studyPlace={'서울 종로구'}
+        studyJoinMember={2}
+        studyMember={4}
+      />
     </>
   );
 }
---
+++ src/components/common/Card.tsx
@@ -0,0 +1,103 @@
+import Image from 'next/image';
+import Label from './Label';
+
+type TCardProps = {
+  studyImage: string;
+  studyMeetings: string;
+  studyTypeisBlue: boolean;
+  studyType: string;
+  studyCategoryisBlue: boolean;
+  studyCatecory: string;
+  studyName: string;
+  studyStart: string;
+  studyEnd: string;
+  studyPlace: string;
+  studyJoinMember: number;
+  studyMember: number;
+};
+export default function Card(props: TCardProps) {
+  const {
+    studyImage,
+    studyMeetings,
+    studyTypeisBlue,
+    studyType,
+    studyCategoryisBlue,
+    studyCatecory,
+    studyName,
+    studyStart,
+    studyEnd,
+    studyPlace,
+    studyJoinMember,
+    studyMember,
+  } = props;
+  return (
+    <div className="w-[18.2rem] h-[25rem]">
+      <div className="relative w-full h-[10rem] mb-[1.2rem]">
+        <Image
+          src={studyImage}
+          width={182}
+          height={100}
+          alt="Card Image"
+          className="rounded-[0.8rem]"
+        />
+        <div className="absolute bottom-0 left-0 right-0 rounded-b-[0.8rem] bg-black bg-opacity-80 text-white text-center text-content-2 p-[0.3rem]">
+          {studyMeetings}
+        </div>
+      </div>
+      <div className="mb-[0.4rem]">
+        <Label isBlue={studyTypeisBlue} content={studyType} />
+        <Label isBlue={studyCategoryisBlue} content={studyCatecory} />
+      </div>
+      <div className="mb-[0.8rem]">
+        <div className="font-bold mt-[0.1rem] mb-[1.2rem]">{studyName}</div>
+        <div className="flex justify-between mb-1 text-gray-600 text-content-2">
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              fill="none"
+            >
+              <path
+                fill="#AFAFAF"
+                d="M6 4.313a.567.567 0 0 1-.563-.563V1.5c0-.308.255-.563.563-.563.308 0 .563.255.563.563v2.25A.567.567 0 0 1 6 4.313ZM12 4.313a.567.567 0 0 1-.563-.563V1.5c0-.308.256-.563.563-.563.307 0 .563.255.563.563v2.25a.567.567 0 0 1-.563.563Z"
+              />
+              <path
+                fill="#AFAFAF"
+                d="M15.75 6.375v6.375c0 2.25-1.125 3.75-3.75 3.75H6c-2.625 0-3.75-1.5-3.75-3.75V6.375c0-2.25 1.125-3.75 3.75-3.75h6c2.625 0 3.75 1.5 3.75 3.75Z"
+                opacity=".4"
+              />
+              <path
+                fill="#AFAFAF"
+                d="M12 8.813H6a.567.567 0 0 1-.563-.563c0-.308.255-.563.563-.563h6c.307 0 .563.255.563.563a.567.567 0 0 1-.563.563ZM9 12.563H6A.567.567 0 0 1 5.437 12c0-.307.255-.563.563-.563h3c.307 0 .563.256.563.563a.567.567 0 0 1-.563.563Z"
+              />
+            </svg>
+            <span>
+              {studyStart}~{studyEnd}
+            </span>
+          </div>
+          <div className="leading-[1.2rem] border-l border-gray-400 "></div>
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              fill="none"
+            >
+              <path
+                fill="#BABABA"
+                d="M9.076 1a6.072 6.072 0 0 0-4.294 1.788A6.118 6.118 0 0 0 3 7.098a6.052 6.052 0 0 0 1.224 3.66s.166.22.193.25l4.659 5.516 4.661-5.518c.025-.03.19-.248.19-.248l.001-.001a6.05 6.05 0 0 0 1.224-3.658 6.118 6.118 0 0 0-1.782-4.31A6.072 6.072 0 0 0 9.076 1Zm0 8.317a2.203 2.203 0 0 1-2.041-1.37 2.226 2.226 0 0 1 .479-2.416 2.207 2.207 0 0 1 3.771 1.568c0 .588-.233 1.152-.648 1.567-.414.416-.975.65-1.561.65Z"
+              />
+            </svg>
+            <span>{studyPlace}</span>
+          </div>
+        </div>
+      </div>
+      <div className="my-2 border-t border-white"></div>
+      <p className="text-gray-700 text-content-2">
+        모집 중 {studyJoinMember}/{studyMember}
+      </p>
+    </div>
+  );
+}
+/* Rectangle 831097 */
