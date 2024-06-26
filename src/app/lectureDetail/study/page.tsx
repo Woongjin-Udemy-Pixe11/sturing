8ab9ebee6ee50063c9660f86ca36acfa06a15ee6
zzzzsb <ji12subin@gmail.com>
2024-06-27 01:46:43 +0900

    [design] 강의 상세 페이지 스터디 탭 페이지
    feat: 새로운 기능 추가
    fix: 버그 수정
    docs: 문서 수정
    design: 퍼블리싱, CSS 등 사용자 UI 디자인 변경
    style: 들여쓰기, 세미콜론 등 코드 스타일 변경
    refactor: 코드 리팩토링
    chore: 외부 라이브러리 임포트 등의 작업 완료
    comment: 필요한 주석 추가 및 변경
    rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    remove: 파일을 삭제하는 작업만 한 경우

---
+++ src/app/lectureDetail/study/page.tsx
@@ -0,0 +1,41 @@
+import { dummyCardList } from '@/dummy/lectureStudy';
+import Card from '@/components/common/Card';
+import SectionNavigator from '@/components/common/SectionNavigator';
+import Link from 'next/link';
+import { IoIosArrowForward } from 'react-icons/io';
+import TitleCountNavigator from '@/components/(jisubin)/TitleCountNavigator';
+
+export default function LectureDetailStudyPage() {
+  return (
+    <>
+      <div className="mt-[5rem]">
+        <TitleCountNavigator
+          title="이 강의를 수강하는 스터디"
+          count={10}
+          moveLink="/"
+        />
+      </div>
+
+      <div className="grid grid-cols-2 gap-x-[0.9rem] gap-y-[2rem] mx-[1.6rem]">
+        {dummyCardList &&
+          dummyCardList.map((card) => (
+            <Card
+              width="167"
+              studyImage={card.studyImage}
+              studyMeetings={card.studyMettings}
+              studyTypeisBlue={card.studyTypeisBlue}
+              studyType={card.studyType}
+              studyCategoryisBlue={card.studyCategoryisBlue}
+              studyCatecory={card.studyCatecory}
+              studyName={card.studyName}
+              studyStart={card.studyStart}
+              studyEnd={card.studyEnd}
+              studyPlace={card.studyPlace}
+              studyJoinMember={card.studyJoinMember}
+              studyMember={card.studyMember}
+            />
+          ))}
+      </div>
+    </>
+  );
+}
