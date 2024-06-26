248e28da8dd79a203df54e3741d3ea8374f101b1
zzzzsb <ji12subin@gmail.com>
2024-06-27 01:28:24 +0900

    [design] 북마크-버튼 하단 네비게이션 바 공통 컴포넌트 분리
    feat: 새로운 기능 추가
    fix: 버그 수정
    docs: 문서 수정
    design: 퍼블리싱, CSS 등 사용자 UI 디자인 변경
    style: 들여쓰기, 세미콜론 등 코드 스타일 변경
    refactor: 코드 리팩토링
    chore: 외부 라이브러리 임포트 등의 작업 완료
    comment: 필요한 주석 추가 및 변경
    rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    remove: 파일을 삭제하는 작업만 한 경우

---
+++ src/components/(jisubin)/BookmarkBtnNavigationBar.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { useState } from 'react';
+import { FaBookmark, FaRegBookmark } from 'react-icons/fa6';
+
+export default function BookmarkBtnNavigationBar() {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const onBookmarkClick = () => {
+    setIsBookmarked(!isBookmarked);
+  };
+  return (
+    <div className="mx-[1.6rem] my-[4.6rem] flex flex-row items-center justify-between">
+      <div className="p-[1.2rem] text-main-600" onClick={onBookmarkClick}>
+        {isBookmarked ? <FaBookmark size={17} /> : <FaRegBookmark size={17} />}
+      </div>
+      <button className="w-[28.7rem] h-[5rem] rounded-[0.5rem] bg-main-600 font-semibold text-white select-none">
+        이 강의로 스터디 개설하기
+      </button>
+    </div>
+  );
+}
