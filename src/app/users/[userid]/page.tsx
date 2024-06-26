16561fbb5d9a22a030d61d8aee769f736fd2bdf8
Moonjonghoo <kafmjh12@gmail.com>
2024-06-26 18:46:31 +0900

    [design] 받은 스터디 평가 페이지 디자인 완료

---
+++ src/app/users/[userid]/page.tsx
@@ -0,0 +1,10 @@
+import StudyReviewCard from './_componets/StudyReviewCard';
+import MyStudyReviewList from './_pages/MyStudyReviewList';
+
+export default function page() {
+  return (
+    <div>
+      <MyStudyReviewList />
+    </div>
+  );
+}
