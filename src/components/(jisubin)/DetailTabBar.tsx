6926bac17052bb7eba2e935084ff561916c99e25
zzzzsb <ji12subin@gmail.com>
2024-06-27 12:10:00 +0900

    [design] 탭바 공통 컴포넌트 분리
    feat: 새로운 기능 추가
    fix: 버그 수정
    docs: 문서 수정
    design: 퍼블리싱, CSS 등 사용자 UI 디자인 변경
    style: 들여쓰기, 세미콜론 등 코드 스타일 변경
    refactor: 코드 리팩토링
    chore: 외부 라이브러리 임포트 등의 작업 완료
    comment: 필요한 주석 추가 및 변경
    rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    remove: 파일을 삭제하는 작업만 한 경우

---
+++ src/components/(jisubin)/DetailTabBar.tsx
@@ -0,0 +1,35 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+type TDetailTabBarProps = {
+  text1: string;
+  text1Link: string;
+  text2: string;
+  text2Link: string;
+  text3: string;
+  text3Link: string;
+};
+export default function DetailTabBar(props: TDetailTabBarProps) {
+  const { text1, text1Link, text2, text2Link, text3, text3Link } = props;
+  const pathname = usePathname();
+  const tabs = [
+    { name: text1, href: text1Link },
+    { name: text2, href: text2Link },
+    { name: text3, href: text3Link },
+  ];
+
+  return (
+    <ul className="w-full px-[1.6rem] flex justify-center items-center text-center border-b-2 text-gray-700 text-content-1">
+      {tabs.map((tab) => (
+        <li
+          key={tab.name}
+          className={`${
+            pathname === tab.href ? 'font-bold text-main-600' : ''
+          } w-[33%] py-[1.2rem]`}
+        >
+          <Link href={tab.href}>{tab.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
