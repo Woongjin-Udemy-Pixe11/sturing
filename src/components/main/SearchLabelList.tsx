7a43f3863c13bfa843ebec5f1b59b010d31a8285
ryukyunghye <ryukyung123@naver.com>
2024-06-27 10:11:33 +0900

    [design] 레이아웃 처리 이후 사이즈, 여백 처리 완료

--- src/components/main/SearchLabelList.tsx
+++ src/components/main/SearchLabelList.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import ScrollableContainer from '../common/ScrollableContainer';
 import { searchLabelList } from '@/constant/searchLabelList';
 
@@ -6,21 +7,23 @@ export default function SearchLabelList() {
     <ScrollableContainer>
       {searchLabelList &&
         searchLabelList.map((label) => (
-          <li
-            key={label.title}
-            className="flex gap-[0.8rem] px-[0.8rem] py-[0.6rem] rounded-[2.5rem] border-2 cursor-pointer"
-          >
-            <figure className="w-[2.4rem] h-[2.4rem] bg-gray-200 rounded-[50%] flex items-center  justify-center">
-              <img
-                src={label.imgSrc}
-                alt={label.imgAlt}
-                className="w-[1.6rem] h-[1.6rem]"
-              />
-              <figcaption className="sr-only">
-                {label.title}를 검색하러 이동합니다.
-              </figcaption>
-            </figure>
-            <span className="whitespace-nowrap">{label.title}</span>
+          <li key={label.title}>
+            <Link
+              href={`/search/result?keyword=${label.title}`}
+              className="flex gap-[0.8rem] px-[0.8rem] py-[0.6rem] rounded-[2.5rem] border-2 cursor-pointer"
+            >
+              <figure className="w-[2.4rem] h-[2.4rem] bg-gray-200 rounded-[50%] flex items-center  justify-center">
+                <img
+                  src={label.imgSrc}
+                  alt={label.imgAlt}
+                  className="w-[1.6rem] h-[1.6rem]"
+                />
+                <figcaption className="sr-only">
+                  {label.title}를 검색하러 이동합니다.
+                </figcaption>
+              </figure>
+              <span className="whitespace-nowrap">{label.title}</span>
+            </Link>
           </li>
         ))}
     </ScrollableContainer>
