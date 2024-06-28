50ea555f2cb6087218794a024c0542fa3cc1a6bf
Moonjonghoo <kafmjh12@gmail.com>
2024-06-28 14:12:28 +0900

    [feat] 컴포넌트 스테이트로 연결 및 상태관리를통한 체크박스, 버튼관리

--- src/app/(공용작업)/test/_component/CollectStudyClient.tsx
+++ src/app/(공용작업)/test/_component/CollectStudyClient.tsx
@@ -8,24 +8,44 @@ import StudyTeammateForm from './StudyTeammateForm';
 
 export default function CollectStudyClient() {
   const [step, setStep] = useState<number>(1);
+  //TODO:any 수정
+  const collectstep: any = {
+    1: <SelectCateGory />,
+    2: <StudyDetailInfoForm />,
+    3: <StudyTeammateForm />,
+  };
   return (
-    <main className="h-100vh">
+    <main>
       <header>
         <h2 className="text-content-1 text-gray-700 p-[1.5rem]">취소</h2>
         <div className="w-full bg-gray-400 rounded-full h-[0.4rem]  ">
           <div
             className="bg-main-500 h-[0.4rem] rounded-full"
-            style={{ width: `${step * 20}%` }}
+            style={{ width: `${step * 33}%` }}
           ></div>
         </div>
       </header>
-      <StudyTeammateForm />
-      <div className="flex m-auto gap-2 w-full p-4">
-        <LongButton color="white" className="w-[40%]">
+      <section className="min-h-[62rem]"> {collectstep[step]}</section>
+
+      <footer className="flex m-auto gap-2 w-full p-4 py-[2rem]">
+        <LongButton
+          color="white"
+          className="w-[40%]"
+          onClick={() => {
+            setStep((prev) => prev - 1);
+          }}
+        >
           이전
         </LongButton>
-        <LongButton color="blue">다음</LongButton>
-      </div>
+        <LongButton
+          color="blue"
+          onClick={() => {
+            setStep((prev) => prev + 1);
+          }}
+        >
+          다음
+        </LongButton>
+      </footer>
     </main>
   );
 }
