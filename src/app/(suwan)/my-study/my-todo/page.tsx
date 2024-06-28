3ba4150a9795e3be6e6aa28d6d094e651920174e
suwan <skdoqj@naver.com>
2024-06-28 13:15:22 +0900

    [design] top import 변경

--- src/app/(suwan)/my-study/my-todo/page.tsx
+++ src/app/(suwan)/my-study/my-todo/page.tsx
@@ -1,10 +1,8 @@
-import Top from '@/components/(suwan)/my-study/top/Top';
 import MyTodoContainer from '@/components/(suwan)/my-study/my-todo/MyTodoContainer';
 
 export default function MyTodo() {
   return (
     <>
-      <Top />
       <MyTodoContainer />
     </>
   );
--- src/app/(suwan)/my-study/team/page.tsx
+++ src/app/(suwan)/my-study/team/page.tsx
@@ -1,10 +1,8 @@
-import Top from '@/components/(suwan)/my-study/top/Top';
 import TeamContainer from '@/components/(suwan)/my-study/team/TeamContainer';
 
 export default function Page() {
   return (
     <>
-      <Top />
       <TeamContainer />
     </>
   );
