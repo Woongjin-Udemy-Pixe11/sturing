3c9f42de41f03d041dcd1f12ed1ee27147ee29b5
kim-yejin <yeje104@naver.com>
2024-06-28 10:20:52 +0900

    [design] 내 스터디 리스트 더미데이터 생성 및 적용

--- src/app/(yejin)/my-study/page.tsx
+++ src/app/(yejin)/my-study/page.tsx
@@ -1,33 +1,18 @@
 import ApplicationList from '@/components/(yejin)/my-study/ApplicationList';
 import StudyList from '@/components/(yejin)/my-study/StudyList';
 import UpcomingStudy from '@/components/(yejin)/my-study/UpcomingStudy';
-import Header from '@/components/common/Header';
+import CreateStudyButton from '@/components/common/CreateStudyButton';
 import TabBar from '@/components/main/TabBar';
-
-const study = [
-  {
-    studyId: 1,
-    studyName: 'UXUI 디자이너 본질 강화 피그마 스터디',
-    studyPlace: '서울시 종로구',
-    studyStart: '07.01(토)',
-    studyEnd: '08.01(토)',
-    studyType: '온라인',
-    studyMember: 4,
-    studyJoinMember: 3,
-    studyScheduleDate: '오후 8:00 - 9:00',
-    studyDaysLeft: 3,
-    studyFormDate: '2024.06.15 14:30',
-  },
-];
+import { study } from '@/dummy/studyList';
 
 export default function page() {
   return (
     <>
-      <Header />
       <TabBar />
       <UpcomingStudy studies={study} />
       <StudyList studies={study} />
       <ApplicationList studies={study} />
+      <CreateStudyButton />
     </>
   );
 }
