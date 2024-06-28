c35dbb7c882d380ab5770d09987cd7bb8dc52824
kim-yejin <yeje104@naver.com>
2024-06-29 02:13:50 +0900

    [design] 스터디 모집글 작성 퍼블리싱

---
+++ src/app/(공용작업)/make-study-form/_component/StudyInfoForm.tsx
@@ -0,0 +1,94 @@
+import StudyForm from '@/components/common/StudyForm';
+import { useState } from 'react';
+import { FiCheck } from 'react-icons/fi';
+
+type TStudyLocationType = 'online' | 'offline' | null;
+
+export default function StudyInfoForm() {
+  const [checked, setChecked] = useState(false);
+  const [locationType, setLocationType] = useState<TStudyLocationType>(null);
+
+  const onChangeCheckBox = () => {
+    setChecked(!checked);
+  };
+
+  const handleLocationChange = (type: TStudyLocationType) => {
+    setLocationType(type);
+  };
+
+  return (
+    <div className="p-[1.6rem]">
+      <StudyForm
+        heading="스터디에 대해 소개해 주세요"
+        imageUpload
+        imageLabel="스터디 대표 사진"
+        titleLabel="스터디 모집글 제목"
+        contentLabel="스터디 소개"
+        titlePlaceholder="내 스터디를 돋보이게 하는 한마디 (최소 5자 이상)"
+        contentPlaceholder="소개글을 입력해 주세요 (최소 20자 필수)"
+        contentMaxLength={250}
+      />
+      <div className="mt-[2rem]">
+        <div className="flex gap-[0.8rem] text-content-1">
+          <button
+            onClick={() => handleLocationChange('online')}
+            className={`px-[1.2rem] py-[0.4rem] rounded-[0.3rem] border border-gray-300 ${
+              locationType === 'online'
+                ? 'bg-main-100 text-main-600 border-main-600'
+                : 'bg-white text-gray-600'
+            }`}
+          >
+            온라인
+          </button>
+          <button
+            onClick={() => handleLocationChange('offline')}
+            className={`px-[1.2rem] py-[0.4rem] rounded-[0.3rem] text-gray-600 border border-gray-300 ${
+              locationType === 'offline'
+                ? 'bg-main-100 text-main-600 border-main-600'
+                : 'bg-white text-gray-600'
+            }`}
+          >
+            오프라인
+          </button>
+        </div>
+
+        {locationType === 'online' && (
+          <div className="mt-[1.2rem]">
+            <input
+              type="text"
+              placeholder="온라인 플랫폼을 입력해주세요. ex)Zoom"
+              className="w-full p-[1.2rem] border rounded-[0.5rem] placeholder:text-content-1 text-gray-500"
+            />
+          </div>
+        )}
+
+        {locationType === 'offline' && (
+          <div className="mt-[1.2rem]">
+            <input
+              type="text"
+              placeholder="오프라인 장소를 입력해주세요."
+              className="w-full p-[1.2rem] border rounded-[0.5rem] placeholder:text-content-1 text-gray-500"
+            />
+          </div>
+        )}
+      </div>
+      <label className="inline-flex items-center space-x-2 py-[1.2rem] relative">
+        <input
+          id="checkbox"
+          type="checkbox"
+          checked={checked}
+          onChange={onChangeCheckBox}
+          className="form-checkbox h-[1.8rem] w-[1.8rem] bg-gray-400 rounded-full  appearance-none checked:bg-main-600 peer "
+        />
+        <label
+          htmlFor="checkbox"
+          className="absolute text-white text-content-2 top-1/2 left-[-2%] transform -translate-y-1/2"
+        >
+          <FiCheck />
+        </label>
+
+        <span className="text-gray-600 text-content-1 text-ge">추후협의</span>
+      </label>
+    </div>
+  );
+}
