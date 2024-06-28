f0b39cfa9324ab2f0fce2c97b3fca035cdb39ec3
suwan <skdoqj@naver.com>
2024-06-28 19:24:35 +0900

    [design] SubHeader 공통 컴포넌트 제작]

---
+++ src/components/common/SubHeader.tsx
@@ -0,0 +1,32 @@
+import { GoChevronLeft } from 'react-icons/go';
+import { IoShareOutline } from 'react-icons/io5';
+import { BsThreeDots } from 'react-icons/bs';
+
+type TSubHeader = {
+  isWhite?: boolean;
+  title?: string;
+  eddit?: boolean;
+  share?: boolean;
+};
+export default function SubHeader(props: TSubHeader) {
+  const { isWhite, title, eddit, share } = props;
+  return (
+    <>
+      <div
+        className={`grid grid-cols-3 items-center h-[5.4rem] px-[1rem] ${
+          isWhite ? `text-white` : ''
+        }`}
+      >
+        <GoChevronLeft size={28} className="" />
+        <div className="text-headline-1 font-medium justify-self-center">
+          {title}
+        </div>
+        <div className="justify-self-end">
+          {share ? <IoShareOutline size={28} /> : ''}
+
+          {eddit ? <BsThreeDots size={28} /> : ''}
+        </div>
+      </div>
+    </>
+  );
+}
