a2095c6a3713f85fd32880898591a2e6a7f8f576
suwan <skdoqj@naver.com>
2024-06-30 02:01:45 +0900

    [design] 팀탭 사진 인증 퍼블리싱

--- src/components/(suwan)/my-study/team/Gallery.tsx
+++ src/components/(suwan)/my-study/team/Gallery.tsx
@@ -2,28 +2,46 @@ export default function Gallery() {
   return (
     <>
       <div className="flex flex-col justify-center items-center w-[90%] mt-[2rem] rounded-[5px] bg-white border border-gray-300">
-        <div className=" bg-white p-6 rounded-lg w-full px-[2rem]">
-          <p className="flex-grow-0 flex-shrink-0 text-base font-semibold text-left text-[#212121]">
-            팀원 사진 인증
-          </p>
-          <svg
-            width={303}
-            height={1}
-            viewBox="0 0 303 1"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="self-stretch flex-grow-0 flex-shrink-0"
-            preserveAspectRatio="xMidYMid meet"
-          >
-            <line x1={303} y1="0.5" y2="0.5" stroke="#E4E4E4" />
-          </svg>
-          <div className="flex flex-wrap">
-            <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[7rem] h-[7rem] relative gap-[1.4rem] p-[0.7rem] rounded-[5px] bg-white border border-[#e3e3e3]"></div>
-            <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[7rem] h-[7rem] relative gap-[1.4rem] p-[0.7rem] rounded-[5px] bg-white border border-[#e3e3e3]"></div>
-            <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[7rem] h-[7rem] relative gap-[1.4rem] p-[0.7rem] rounded-[5px] bg-white border border-[#e3e3e3]"></div>
-            <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[7rem] h-[7rem] relative gap-[1.4rem] p-[0.7rem] rounded-[5px] bg-white border border-[#e3e3e3]"></div>
-            <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[7rem] h-[7rem] relative gap-[1.4rem] p-[0.7rem] rounded-[5px] bg-white border border-[#e3e3e3]"></div>
-            <div className="flex justify-center items-center flex-grow-0 flex-shrink-0 w-[7rem] h-[7rem] relative gap-[1.4rem] p-[0.7rem] rounded-[5px] bg-white border border-[#e3e3e3]"></div>
+        <div className=" bg-white p-6 rounded-lg w-full px-[2rem] ">
+          <div className="flex items-center border-b-[0.1rem] border-gray-300 pb-4">
+            <h2 className="text-[1.6rem] font-semibold">팀원 사진 인증</h2>
+          </div>
+
+          <div className="grid grid-rows-2 grid-cols-3 gap-[.4rem] py-[2rem] w-[100%]">
+            <div className="relative w-[100%] h-[100%] rounded-[.5rem] border-2 border-gray-300 after:pb-[100%] after:block">
+              <div className="absolute h-[100%] w-[100%] rounded-[.5rem] object-cover;">
+                이미지
+              </div>
+            </div>
+            <div className="relative w-[100%] h-[100%] rounded-[.5rem] border-2 border-gray-300 after:pb-[100%] after:block">
+              <div className="absolute h-[100%] w-[100%] rounded-[.5rem] object-cover;">
+                이미지
+              </div>
+            </div>
+            <div className="relative w-[100%] h-[100%] rounded-[.5rem] border-2 border-gray-300 after:pb-[100%] after:block">
+              <div className="absolute h-[100%] w-[100%] rounded-[.5rem] object-cover;">
+                이미지
+              </div>
+            </div>
+            <div className="relative w-[100%] h-[100%] rounded-[.5rem] border-2 border-gray-300 after:pb-[100%] after:block">
+              <div className="absolute h-[100%] w-[100%] rounded-[.5rem] object-cover;">
+                이미지
+              </div>
+            </div>
+            <div className="relative w-[100%] h-[100%] rounded-[.5rem] border-2 border-gray-300 after:pb-[100%] after:block">
+              <div className="absolute h-[100%] w-[100%] rounded-[.5rem] object-cover;">
+                이미지
+              </div>
+            </div>
+            <div className="relative w-[100%] h-[100%] rounded-[.5rem] border-2 border-gray-300 after:pb-[100%] after:block">
+              <div className="absolute h-[100%] w-[100%] rounded-[.5rem] object-cover;">
+                이미지
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full flex justify-center">
+            <img src="/images/studyLabel/arrow_down.svg" />
           </div>
         </div>
       </div>
