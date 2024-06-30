4d004f921efff1954ffdecb4b11c7184461bfebf
Moonjonghoo <kafmjh12@gmail.com>
2024-07-01 01:18:14 +0900

    [feat] my-study-main,layout 1레벨하위부분만 레이아웃적용될수있게 변경 - 레이아웃 클라이언트 컴포넌트 수정 - 1레벨 하위부분만 레이아웃 적용 - 더 밑에부터는 레이아웃 적용 안됨. feat: 새로운 기능 추가 fix: 버그 수정 docs: 문서 수정 design: 퍼블리싱, CSS 등 사용자 UI 디자인 변경 style: 들여쓰기, 세미콜론 등 코드 스타일 변경 refactor: 코드 리팩토링 chore: 외부 라이브러리 임포트 등의 작업 완료 comment: 필요한 주석 추가 및 변경 rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우 remove: 파일을 삭제하는 작업만 한 경우

--- src/app/(공용작업)/my-study-main/layout.tsx
+++ src/app/(공용작업)/my-study-main/layout.tsx
@@ -1,18 +1,25 @@
+'use client';
 import Header from '@/components/(suwan)/my-study/common/Header';
 import TabBar from '@/components/(suwan)/my-study/head/Tabar';
 import StudyInfo from '@/components/(suwan)/my-study/head/StudyInfo';
 import { study } from '@/dummy/studyList';
-
-export default function Layout({ children }) {
+import { useSelectedLayoutSegments } from 'next/navigation';
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const segement = useSelectedLayoutSegments();
+  let isHeader = segement.length === 1 ? true : false;
   return (
     <>
-      <Header />
-      <StudyInfo />
-      <TabBar />
-      <img
-        className="absolute inset-0 w-full h-full object-cover -z-10"
-        src="/images/study-img1.png"
-      />
+      {isHeader && (
+        <>
+          <Header />
+          <StudyInfo />
+          <TabBar />
+          <img
+            className="absolute inset-0 w-full h-full object-cover -z-10"
+            src="/images/study-img1.png"
+          />
+        </>
+      )}
       <main>{children}</main>
     </>
   );
