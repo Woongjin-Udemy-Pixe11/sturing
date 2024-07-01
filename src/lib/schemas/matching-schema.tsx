24940cc2e12c475586db5209f2b2c9a2c162c7cb
Moonjonghoo <kafmjh12@gmail.com>
2024-07-01 15:09:58 +0900

    [feat] 유저 매칭정보 스키마 파일 추가

---
+++ src/lib/schemas/matching-schema.tsx
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+const matchingSchema = new mongoose.Schema({
+  userid: { type: String },
+  interests: { type: Array },
+  level: { type: Object },
+  studyType: { type: String },
+  preferRegion: { type: Array },
+  preferMood: { type: Array },
+});
+export const Matching =
+  mongoose.models?.Matching || mongoose.model('Matching', matchingSchema);
