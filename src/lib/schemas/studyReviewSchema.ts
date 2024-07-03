9ea5a9bb17799d46485afdb272e927b70edc8600
kim-yejin <yeje104@naver.com>
2024-07-03 20:39:42 +0900

    [feat] 예진 담당 스키마 작성 완료

---
+++ src/lib/schemas/studyReviewSchema.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+const studyReviewSchema = new mongoose.Schema(
+  {
+    studyId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'study',
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    studyReviewScore: {
+      type: Number,
+      required: true,
+      default: 1,
+      enum: [1, 3, 5], // 별로예요(1), 좋아요(3), 최고예요(5)
+    },
+    studyReviewContent: {
+      type: String,
+      required: true,
+      maxlength: 500,
+      immutable: true,
+    },
+  },
+  { timestamps: true },
+);
+export const StudyReview =
+  mongoose.models?.StudyReview ||
+  mongoose.model('study_review', studyReviewSchema);
--- src/lib/schemas/studySchema.ts
+++ src/lib/schemas/studySchema.ts
@@ -1,17 +1,92 @@
 import mongoose from 'mongoose';
+const studySchema = new mongoose.Schema(
+  {
+    leaderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    studyImage: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    studyName: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 30,
+    },
+    studyContent: {
+      type: String,
+      required: true,
+      minlength: 20,
+      maxlength: 500,
+    },
+    studyType: {
+      type: String,
+      required: true,
+      enum: ['온라인', '오프라인', '추후협의'],
+    },
+    studyLevel: {
+      type: String,
+      required: true,
+      enum: ['비기너', '신입', '주니어', '시니어', '상관없음'],
+    },
+    studyMember: {
+      type: Number,
+      required: true,
+    },
+    studyJoinMember: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    studySubject: {
+      type: String,
+    },
+    studyCategory: {
+      type: String,
+      enum: [
+        '디자인',
+        '개발테크',
+        '비즈니스',
+        '마케팅',
+        '경제',
+        '외국어',
+        '자격증',
+        '자기계발',
+        '기타',
+      ],
+    },
+    studyViews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    studyDeadline: {
+      type: Date,
+      required: true,
+    },
+    studyStart: {
+      type: Date,
+      required: true,
+    },
+    studyEnd: {
+      type: Date,
+      required: true,
+    },
+    studyPlace: {
+      type: String,
+      required: true,
+    },
+    studyMeetings: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
-const studyMemberSchema = new mongoose.Schema({
-  studyID: [
-    { type: mongoose.Schema.Types.ObjectId, require: true, ref: 'study' },
-  ],
-  userID: { type: String },
-  study_progress: { type: Number },
-});
-const studySchema = new mongoose.Schema({
-  leaderID: { type: String },
-});
-
-const memberModel = mongoose.model('member', studyMemberSchema);
-const studyModel = mongoose.model('study', studySchema);
-
-const member = memberModel.find({}).populate('study');
+export const Study =
+  mongoose.models?.Study || mongoose.model('study', studySchema);
