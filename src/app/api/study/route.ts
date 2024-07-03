8c33539452f2003519fddf27c3601802bda5980c
kim-yejin <yeje104@naver.com>
2024-07-03 20:43:47 +0900

    [feat] study route 추가

---
+++ src/app/api/study/route.ts
@@ -0,0 +1,32 @@
+import connectDB from '@/lib/db';
+import { Study } from '@/lib/schemas/studySchema';
+
+connectDB();
+
+async function getStudies(sort?: string | null) {
+  let studies = Study.find();
+  if (sort === 'popular') {
+    studies = studies.sort({ studyViews: -1 }).limit(3);
+  } else if (sort === 'recent') {
+    studies = studies.sort({ createdAt: -1 }).limit(3);
+  }
+  return await studies.exec();
+}
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const sort = searchParams.get('sort');
+  try {
+    const studies = await getStudies(sort);
+    return Response.json(studies);
+  } catch (error) {
+    console.log(error);
+    return Response.json({ status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  const res = await request.json();
+  const studies = await Study.create(res);
+  return Response.json(studies);
+}
