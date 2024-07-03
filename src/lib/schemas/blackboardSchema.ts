50af1cc65babb8e28d58b9ba2aedc4c9dd28dd5e
suwan <skdoqj@naver.com>
2024-07-03 17:14:11 +0900

    [feat] 수완 담당 스키마 작성 완료

---
+++ src/lib/schemas/blackboardSchema.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const blackboardSchema = new mongoose.Schema({
+  blackboardWriteId: {
+    type: mongoose.Schema.Types.ObjectId,
+    require: true,
+    ref: 'user',
+  },
+  blackboardTitle: { type: String, required: true },
+  blackboardContent: { type: String, required: true },
+  blackboardImage: { type: String, required: true },
+  blackboardViews: { type: Number, required: true, default: 0 },
+  blackboardDate: { type: Date, required: true },
+});
+
+export const Blackboard =
+  mongoose.models?.Blackboard || mongoose.model('blackboard', blackboardSchema);
