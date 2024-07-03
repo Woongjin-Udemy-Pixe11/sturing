e7b9eabb8e585cc2623f64c9e982487547aebfb3
kim-yejin <yeje104@naver.com>
2024-07-03 20:42:59 +0900

    [design] 스터디 타입 변경

---
+++ src/types/TStudy.ts
@@ -0,0 +1,16 @@
+// Study 타입 정의
+export type TStudy = {
+  studyId?: string;
+  studyImage: string;
+  studyMeetings: string;
+  studyTypeisBlue?: boolean;
+  studyType: string;
+  studyCategoryisBlue?: boolean;
+  studyCategory: string;
+  studyName: string;
+  studyStart: string;
+  studyEnd: string;
+  studyPlace: string;
+  studyJoinMember: number;
+  studyMember: number;
+};
