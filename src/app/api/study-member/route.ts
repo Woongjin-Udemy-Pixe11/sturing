e2eb580974e3bb9f8f90b76c5c60b3e8ee20ddf6
kim-yejin <yeje104@naver.com>
2024-07-10 20:28:29 +0900

    [feat] 스터디 멤버 불러오기 기능 추가

--- src/app/api/study-member/route.ts
+++ src/app/api/study-member/route.ts
@@ -1,8 +1,35 @@
 import connectDB from '@/lib/db';
 import { StudyMember } from '@/lib/schemas/studyMemberSchema';
+import mongoose from 'mongoose';
 
-export async function GET(req: Request) {
-  return Response.json({ message: 'GET / api/studyMember' });
+export async function GET(request: Request) {
+  await connectDB();
+
+  const { searchParams } = new URL(request.url);
+  const studyId = searchParams.get('studyId');
+  console.log('Received studyId:', studyId);
+
+  if (!studyId) {
+    return Response.json({ error: 'studyId is required' }, { status: 400 });
+  }
+
+  try {
+    const studyMembers = await StudyMember.find({
+      studyId: new mongoose.Types.ObjectId(studyId),
+    })
+      .populate('userId', 'nickname')
+      .lean();
+
+    console.log('Found study members:', studyMembers);
+
+    return Response.json(studyMembers);
+  } catch (error) {
+    console.error('Error fetching study members:', error);
+    return Response.json(
+      { error: 'Failed to fetch study members' },
+      { status: 500 },
+    );
+  }
 }
 
 export async function POST(request: Request) {
