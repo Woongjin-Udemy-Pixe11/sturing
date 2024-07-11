7ae6e1634a18b26ca8808e8b8c92778f2d5d1ea9
kim-yejin <yeje104@naver.com>
2024-07-12 02:15:15 +0900

    [feat] 공통 모달 버튼 함수 추가

--- src/components/common/modal/DefaultModal.tsx
+++ src/components/common/modal/DefaultModal.tsx
@@ -1,9 +1,11 @@
 type TModalProps = {
   message: string;
+  onConfirm?: () => void;
+  onCancel?: () => void;
 };
 
 export default function DefaultModal(props: TModalProps) {
-  const { message } = props;
+  const { message, onConfirm, onCancel } = props;
 
   const buttonStyle = 'h-[3.3rem] px-[1.2rem] rounded-[0.3rem] text-content-1';
   return (
@@ -13,10 +15,14 @@ export default function DefaultModal(props: TModalProps) {
           {message}
         </span>
         <div className="flex gap-[0.6rem]">
-          <button className={` bg-main-600 text-white ${buttonStyle}`}>
+          <button
+            onClick={onConfirm}
+            className={` bg-main-600 text-white ${buttonStyle}`}
+          >
             예
           </button>
           <button
+            onClick={onCancel}
             className={` bg-white border-[0.1rem] border-main-600 text-main-600 ${buttonStyle}`}
           >
             아니오
