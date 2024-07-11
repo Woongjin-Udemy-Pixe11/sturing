8331473ef02a86121e18cbc5c250b64c237f3b69
suwan <skdoqj@naver.com>
2024-07-11 15:03:21 +0900

    [feat] 스터디 개설: 선택한 강의 fetch 받기

--- src/app/(suwan)/make-study-form/[lectureId]/page.tsx
+++ src/app/(suwan)/make-study-form/[lectureId]/page.tsx
@@ -8,7 +8,23 @@ export default async function page(props: any) {
   const session = await getSession();
   const id = session?.user?.id;
 
+  const lectureName = await (
+    await fetch(
+      `http://localhost:3000/api/lecture/${lectureId}?data=lectureName`,
+    )
+  ).json();
+
+  console.log(lectureName);
+
   return (
-    <>{id && <CollectStudyClient leaderId={id} lectureId={lectureId} />}</>
+    <>
+      {id && (
+        <CollectStudyClient
+          leaderId={id}
+          lectureId={lectureId}
+          lectureName={lectureName}
+        />
+      )}
+    </>
   );
 }
