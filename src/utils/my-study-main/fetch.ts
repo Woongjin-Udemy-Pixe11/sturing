da0b00802ff4783c75e81381e6f4e66b55f57dad
suwan <skdoqj@naver.com>
2024-07-13 17:52:40 +0900

    [feat] my-study-main 출석기능

--- src/utils/my-study-main/fetch.ts
+++ src/utils/my-study-main/fetch.ts
@@ -1,12 +1,20 @@
+export async function fetchLecture(lectureId: any) {
+  const res = await fetch(
+    `${process.env.LOCAL_URL}/api/lecture/${lectureId}?data=lectureName&data=lectureURL`,
+  );
+
+  return res.json();
+}
+
 export async function fetchStudy(studyId: any) {
   const res = await fetch(`${process.env.LOCAL_URL}/api/study?id=${studyId}`);
 
   return res.json();
 }
 
-export async function fetchLecture(lectureId: any) {
+export async function fetchMember(studyId: any) {
   const res = await fetch(
-    `${process.env.LOCAL_URL}/api/lecture/${lectureId}?data=lectureName&data=lectureURL`,
+    `${process.env.LOCAL_URL}/api/study-member?studyId=${studyId}`,
   );
 
   return res.json();
