17b2e8ec621ea1f67ee474877dd2376743780862
kim-yejin <yeje104@naver.com>
2024-07-16 14:30:20 +0900

    [feat] 날짜, 시간 보여지는 형식 변경

--- src/components/(yejin)/my-study/UpcomingStudy.tsx
+++ src/components/(yejin)/my-study/UpcomingStudy.tsx
@@ -32,6 +32,21 @@ export default function UpcomingStudy(props: StudyProps) {
     return diffDays;
   };
 
+  // 시간만 뺴오는 함수
+  const formatTime = (timeString: string) => {
+    if (timeString.includes('추후협의')) {
+      return '추후협의';
+    }
+    // 요일만 추출 (첫 번째 단어)
+    const [day, time] = timeString.split(' ');
+    const [hour, minute] = time.split(':');
+    const endHour = (parseInt(hour) + 1) % 24;
+
+    return `${hour}:${minute} ~ ${endHour
+      .toString()
+      .padStart(2, '0')}:${minute}`;
+  };
+
   return (
     <div className="w-full bg-gradient-to-tr from-[rgba(217,227,255,0.4)] to-[rgba(255,228,224,0.4)] px-[1.6rem] py-[2.8rem]">
       <h2 className="font-semibold text-[2.0rem] mb-[2rem]">다가오는 스터디</h2>
@@ -52,7 +67,7 @@ export default function UpcomingStudy(props: StudyProps) {
             upcomingStudies.map((study) => (
               <SwiperSlide key={study._id}>
                 <Link
-                  href="/my-study-main/team"
+                  href="/my-study-main/{study._id}/team"
                   className="bg-white w-full flex flex-col gap-[0.8rem] px-[2rem] py-[2.4rem] rounded-[0.8rem] border border-gray-300"
                 >
                   <div className="[&>span]:ml-0 [&>span]:mr-[0.4rem]">
@@ -68,7 +83,10 @@ export default function UpcomingStudy(props: StudyProps) {
                   <div className="flex bg-gray-100 p-[0.8rem] gap-[0.8rem] [&>span]:text-content-2 rounded-[0.3rem]">
                     <span className="text-gray-700">{study.studyPlace}</span>
                     <span className="text-gray-400">|</span>
-                    <span className="text-gray-700">{study.studyMeetings}</span>
+                    <span className="text-gray-700">
+                      {formatDate2(study.studyStart) + ' '}
+                      {formatTime(study.studyMeetings)}
+                    </span>
                   </div>
                 </Link>
               </SwiperSlide>
