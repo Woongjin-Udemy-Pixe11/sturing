8efd9f38d22b9d5de860c8fb8c4735a3a8898a8b
suwan <skdoqj@naver.com>
2024-07-21 03:47:56 +0900

    [remove] 스터디스케줄 파일 구조 수정

--- src/app/(suwan)/my-study-main/[studyId]/calendar/page.tsx
+++ src/app/(suwan)/my-study-main/[studyId]/calendar/page.tsx
@@ -1,6 +1,16 @@
-import CalendarContainer from '@/components/(suwan)/my-study/calendar/CalendarContainer';
 import Header from '../_components/Header';
 import { fetchStudy } from '@/utils/my-study-main/fetch';
+import Calendar from '@/components/(suwan)/my-study/common/Calendar';
+import Schedule from './_components/Schedule';
+
+const schedule = {
+  leaderId: '668c8cc789017e9fea0f9dc4',
+  title: '등촌역 집합',
+  place: '등촌동',
+  date: '',
+  time: '오후 8:00 - 9:00',
+  studyId: '6697daf94cb179ed2b8159cd',
+};
 
 export default async function Calender({
   params,
@@ -13,7 +23,9 @@ export default async function Calender({
   return (
     <>
       <Header studyId={studyId} data={data} />
-      <CalendarContainer />
+      {/* <CalendarContainer /> */}
+      <Calendar studyId={''} userId={''} data={schedule} />
+      <Schedule />
     </>
   );
 }
