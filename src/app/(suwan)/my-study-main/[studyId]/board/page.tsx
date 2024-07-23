7241b11878eda3b88fce5fb9f8d5943d042764ea
suwan <skdoqj@naver.com>
2024-07-24 00:02:24 +0900

    [design] css 수정 및 Link영역 확대

--- src/app/(suwan)/my-study-main/[studyId]/board/page.tsx
+++ src/app/(suwan)/my-study-main/[studyId]/board/page.tsx
@@ -1,9 +1,9 @@
-import SectionNavigator from '@/components/common/SectionNavigator';
 import TaskInfo from './_component/TaskInfo';
 import Header from '../_components/Header';
 import { fetchBoardList, fetchStudy } from '@/utils/my-study-main/fetch';
 import { getSession } from '@/utils/getSessions';
 import Link from 'next/link';
+import { IoIosArrowForward } from 'react-icons/io';
 
 export default async function page({
   params,
@@ -25,36 +25,39 @@ export default async function page({
       <Header studyId={studyId} data={data} />
 
       <div className="bg-gray-100 py-[2.4rem]">
-        <div className="bg-white rounded-[0.5rem] border-gray-300 border-[0.1rem] mx-[1.6rem] my-[2.4rem] pb-[2.4rem]">
-          <div className="mx-[1.6rem]">
-            <SectionNavigator
-              title="공지사항"
-              moveLink={`./board/notice-board`}
-            />
-            <hr className="mx-[0.4rem] mb-[1.2rem] border-b-gray-300 border-b-1"></hr>
-            {noticeList.length > 0 && (
-              <Link href={`./board/notice-board/${noticeList[0]._id}`}>
-                <div className="flex flex-row items-center gap-x-[0.8rem] pb-[2.4rem] px-[0.4rem]">
-                  <div className="min-w-[3.3rem] min-h-[2.2rem] px-[0.6rem] py-[0.2rem] bg-main-100 rounded-[0.3rem] text-main-700 text-content-2 break-all">
-                    필독
-                  </div>
-                  <span className="text-content-2 truncate">
-                    {noticeList[0].title}
-                  </span>
+        <div className="mx-[1.6rem] my-[2.4rem] px-[2rem] py-[2.4rem] bg-white rounded-[0.5rem] border-gray-300 border-[0.1rem]  ">
+          <Link href={`./board/notice-board`}>
+            <div className="pb-[1.4rem] pr-[1rem] w-full flex justify-between items-center text-headline-3 font-semibold">
+              <span>공지사항</span>
+              <IoIosArrowForward />
+            </div>
+          </Link>
+
+          <hr className="border-b-gray-300 border-b-1"></hr>
+          {noticeList.length > 0 && (
+            <Link href={`./board/notice-board/${noticeList[0]._id}`}>
+              <div className="flex flex-row items-center gap-x-[0.8rem] py-[1.6rem]">
+                <div className="min-w-[3.3rem] min-h-[2.2rem] px-[0.6rem] py-[0.2rem] bg-main-100 rounded-[0.3rem] text-main-700 text-content-2 break-all">
+                  필독
                 </div>
-              </Link>
-            )}
-          </div>
+                <span className="text-content-2 truncate">
+                  {noticeList[0].title}
+                </span>
+              </div>
+            </Link>
+          )}
         </div>
 
-        <div className="bg-white rounded-[0.5rem] border-gray-300 border-[0.1rem] mx-[1.6rem] my-[2.4rem] pb-[2.4rem]">
-          <div className="mx-[1.6rem]">
-            <SectionNavigator
-              title="과제 게시판"
-              moveLink={`/my-study-main/${studyId}/board/task-board`}
-            />
-            <hr className="mx-[0.4rem] mb-[1.2rem] border-b-gray-300 border-b-1"></hr>
-          </div>
+        <div className="bg-white rounded-[0.5rem] border-gray-300 border-[0.1rem] mx-[1.6rem] my-[2.4rem] px-[2rem] py-[2.4rem]">
+          <Link href={`/my-study-main/${studyId}/board/task-board`}>
+            <div className="pb-[1.4rem] pr-[1rem] w-full flex justify-between items-center text-headline-3 font-semibold">
+              <span>과제 게시판</span>
+              <IoIosArrowForward />
+            </div>
+          </Link>
+
+          <hr className="border-b-gray-300 border-b-1"></hr>
+
           {taskList.length > 0 &&
             taskList.slice(0, 2).map((task: any) => (
               <Link href={`./board/task-board/${task._id}`}>
