96a8675feda690635a4e32d3641a6ab93dfee90f
suwan <skdoqj@naver.com>
2024-07-25 05:22:36 +0900

    [feat] 일반스터디 개설

--- src/app/(suwan)/make-study-form/complete/page.tsx
+++ src/app/(suwan)/make-study-form/complete/page.tsx
@@ -12,7 +12,7 @@ export default function Completed() {
         subheading="스터디 지원자들의 지원서는 내 스터디에서
 확인하고 수락할 수 있어요."
       />
-      <div onClick={() => router.push('/')}>
+      <div onClick={() => router.push('/')} className="mx-[1.6rem]">
         <LongButton color="blue">확인</LongButton>
       </div>
     </>
