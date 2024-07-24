ce676a5314955b60c10249111d06d0bab5a08760
zzzzsb <ji12subin@gmail.com>
2024-07-25 00:16:23 +0900

    [fix] 검색 결과 페이지 뒤로가기 버튼 기능 추가
    feat: 새로운 기능 추가
    fix: 버그 수정
    docs: 문서 수정
    design: 퍼블리싱, CSS 등 사용자 UI 디자인 변경
    style: 들여쓰기, 세미콜론 등 코드 스타일 변경
    refactor: 코드 리팩토링
    chore: 외부 라이브러리 임포트 등의 작업 완료
    comment: 필요한 주석 추가 및 변경
    rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    remove: 파일을 삭제하는 작업만 한 경우

--- src/app/(ryukyung)/search/result/page.tsx
+++ src/app/(ryukyung)/search/result/page.tsx
@@ -4,8 +4,7 @@ import TabContainer from '@/components/search/TabContainer';
 import TabLecture from '@/components/search/TabLecture';
 import TabStudy from '@/components/search/TabStudy';
 import { getFilteredResults } from '@/lib/actions/filterAction';
-import { IoIosArrowBack } from 'react-icons/io';
-import SearchPart from '../../pages/SearchPart';
+import BackSearchBar from './_components/BackSearchBar';
 
 export default async function page({ searchParams }: { searchParams: any }) {
   const keyword = searchParams.keyword;
@@ -60,14 +59,7 @@ export default async function page({ searchParams }: { searchParams: any }) {
   ];
   return (
     <>
-      <div className="w-full flex items-center px-[1.2rem] pb-[2rem]">
-        <button className="flex-shrink-0 mt-[2.4rem]">
-          <IoIosArrowBack className="w-[2.4rem] h-[2.4rem]" />
-        </button>
-        <div className="flex-grow max-w-[97%]">
-          <SearchPart isList={false} />
-        </div>
-      </div>
+      <BackSearchBar />
       <TabContainer
         keyword={keyword}
         tabList={tabList}
