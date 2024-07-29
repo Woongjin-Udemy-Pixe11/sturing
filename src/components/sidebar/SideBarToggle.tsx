948695220063dee01fb6f39e38ca8249bb6af85a
kim-yejin <yeje104@naver.com>
2024-07-30 03:24:53 +0900

    [design] 사이드바 간격 수정

--- src/components/sidebar/SideBarToggle.tsx
+++ src/components/sidebar/SideBarToggle.tsx
@@ -12,27 +12,30 @@ export default function SideBarToggle() {
   return (
     <>
       <div>
-        <p className="w-full flex items-center justify-between text-headline-3 mb-[1rem]">
-          분야
-          <button>
-            {isOpen && <IoIosArrowDown onClick={clickToggleButton} />}
-            {!isOpen && <IoIosArrowUp onClick={clickToggleButton} />}
-          </button>
-        </p>
+        <button
+          onClick={clickToggleButton}
+          className="w-full flex items-center justify-between text-headline-3"
+        >
+          <p>분야</p>
+          <span>
+            {isOpen && <IoIosArrowDown />}
+            {!isOpen && <IoIosArrowUp />}
+          </span>
+        </button>
         {isOpen && (
-          <ul className="w-full flex flex-wrap gap-[.4rem]">
+          <ul className="w-full flex flex-wrap gap-[.8rem] pt-[1.6rem]">
             {searchLabelList &&
               searchLabelList.map((label) => (
                 <Link
                   href={`/search/result?field=${label.title}`}
                   key={label.title}
                 >
-                  <li className="flex gap-[0.8rem] px-[0.8rem] py-[0.6rem] rounded-[2.5rem] border border-gray-400 cursor-pointer">
-                    <figure className="w-[2.4rem] h-[2.4rem] bg-gray-200 rounded-[50%] flex items-center  justify-center">
+                  <li className="flex gap-[0.4rem] px-[.9rem] py-[.8rem] rounded-[2.5rem] border border-gray-400 cursor-pointer text-content-1 tracking-[-0.03rem] items-center justify-center">
+                    <figure className="w-[2.2rem] h-[2.2rem] bg-gray-200 rounded-[50%] flex items-center justify-center">
                       <img
                         src={label.imgSrc}
                         alt={label.imgAlt}
-                        className="w-[1.6rem] h-[1.6rem]"
+                        className="w-[1.5rem] h-[1.5rem]"
                       />
                       <figcaption className="sr-only">
                         {label.title}를 검색하러 이동합니다.
