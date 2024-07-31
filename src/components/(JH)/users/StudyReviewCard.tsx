840a66b5c3f8ddd585dea56e667f436ddb0a7cc0
zzzzsb <ji12subin@gmail.com>
2024-08-01 05:49:54 +0900

    [feat] 받은 스터디 평가 페이지에서 유저 이미지, 닉네임 클릭시 해당 유저 마이페이지(남이보는) 이동
    feat: 새로운 기능 추가
    fix: 버그 수정
    docs: 문서 수정
    design: 퍼블리싱, CSS 등 사용자 UI 디자인 변경
    style: 들여쓰기, 세미콜론 등 코드 스타일 변경
    refactor: 코드 리팩토링
    chore: 외부 라이브러리 임포트 등의 작업 완료
    comment: 필요한 주석 추가 및 변경
    rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    remove: 파일을 삭제하는 작업만 한 경우

--- src/components/(JH)/users/StudyReviewCard.tsx
+++ src/components/(JH)/users/StudyReviewCard.tsx
@@ -1,4 +1,5 @@
 import { TstudyReview } from '@/types/TStudyReview';
+import Link from 'next/link';
 
 type TstudyReviewCardProps = {
   review: TstudyReview;
@@ -8,14 +9,18 @@ export default function StudyReviewCard(props: TstudyReviewCardProps) {
   const { review } = props;
   return (
     <section className="flex gap-[0.8rem]  border-b border-gray-300 pb-[2rem] w-full">
-      <div className="w-[4rem] h-[4rem] border border-gray-500 rounded-full overflow-hidden">
-        <img
-          src={`${review.userId.image}`}
-          className="object-cover h-full"
-        ></img>
-      </div>
+      <Link href={`/users/${review.userId._id}`}>
+        <div className="w-[4rem] h-[4rem] border border-gray-500 rounded-full overflow-hidden">
+          <img
+            src={`${review.userId.image}`}
+            className="object-cover h-full"
+          ></img>
+        </div>
+      </Link>
       <div className="w-[29.7rem] flex flex-col gap-[.6rem] text-content-1">
-        <h1 className="font-semibold ">{review.userId.nickname}</h1>
+        <Link href={`/users/${review.userId._id}`}>
+          <h1 className="font-semibold ">{review.userId.nickname}</h1>
+        </Link>
         <p>{review.studyReviewContent}</p>
       </div>
     </section>
