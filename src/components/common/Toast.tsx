093f7a4125875f70d57c0348187ac0caa4e72fd9
zzzzsb <ji12subin@gmail.com>
2024-08-01 17:07:56 +0900

    [design] 클립보드가 복사되었습니다 메시지 헤더 위로 오도록 z-index 수정
    feat: 새로운 기능 추가
    fix: 버그 수정
    docs: 문서 수정
    design: 퍼블리싱, CSS 등 사용자 UI 디자인 변경
    style: 들여쓰기, 세미콜론 등 코드 스타일 변경
    refactor: 코드 리팩토링
    chore: 외부 라이브러리 임포트 등의 작업 완료
    comment: 필요한 주석 추가 및 변경
    rename: 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우
    remove: 파일을 삭제하는 작업만 한 경우

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -7,7 +7,7 @@ export default function Toast(props: TToastProps) {
 
   return (
     <>
-      <div className="fixed top-10 left-1/2 transform -translate-x-1/2 bg-[#555] bg-opacity-95 w-[70%] text-center px-[2rem] py-[1rem] rounded-[0.8rem] text-white">
+      <div className="fixed top-10 left-1/2 transform -translate-x-1/2 bg-[#555] bg-opacity-95 w-[70%] text-center px-[2rem] py-[1rem] rounded-[0.8rem] text-white z-50">
         <span className="text-text-caption text-content-1">{message}</span>
       </div>
     </>
