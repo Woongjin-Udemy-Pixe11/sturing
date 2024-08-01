08f0f6cbc346434b3a81f297d5fa3c4794def3b5
suwan <skdoqj@naver.com>
2024-08-02 03:14:00 +0900

    [feat] 스터디 메인 뒤로가기 링크 추가

--- src/app/my-study-main/[studyId]/_components/Header.tsx
+++ src/app/my-study-main/[studyId]/_components/Header.tsx
@@ -1,7 +1,10 @@
 'use server';
 import SubHeader from '@/components/common/SubHeader';
-import TabBar from '@/components/my-study-main/head/Tabar';
+
+import { GoChevronLeft } from 'react-icons/go';
+import Link from 'next/link';
 import StudyInfo from '@/components/my-study-main/head/StudyInfo';
+import TabBar from '@/components/my-study-main/head/Tabar';
 
 export default async function Header({ studyId, data }: any) {
   return (
@@ -12,7 +15,14 @@ export default async function Header({ studyId, data }: any) {
         src={data.studyImage}
       />
 
-      <SubHeader isWhite={true} />
+      <Link
+        href="/my-study-list"
+        className="flex h-[5.4rem] pl-[1rem] items-center text-white relative"
+      >
+        <GoChevronLeft className="text-[2.8rem]" />
+      </Link>
+
+      {/* <SubHeader isWhite={true} /> */}
       <StudyInfo data={data} />
       <TabBar studyId={studyId} />
     </>
